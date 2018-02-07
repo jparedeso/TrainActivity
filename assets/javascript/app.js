5cb1a3bf74b143d3bd1c027bc3ec45712327c76d
jparedeso <jparedeso@gmail.com>
2018-02-07 14:45:31 -0500

    Added function to show train data on HTML using template string.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,40 +1,59 @@
-var LoginPage = function() {
-    function init() {
-         // Initialize Firebase
-      var config = {
+var TrainActivity = function() {
+
+    var trainName = "";
+    var destination = "";
+    var firstTrainTime = "";
+    var frequency = "";
+    var newData;
+
+       // Initialize Firebase
+    var config = {
         apiKey: "AIzaSyAp6lBndBnWQIXB1uoGZEpJISR3YPKK8pM",
         authDomain: "trainactivity-d3485.firebaseapp.com",
         databaseURL: "https://trainactivity-d3485.firebaseio.com",
         projectId: "trainactivity-d3485",
         storageBucket: "",
         messagingSenderId: "347867649628"
-      };
-      firebase.initializeApp(config);
+    };
+    firebase.initializeApp(config);
 
-        // FirebaseUI config.
-        var uiConfig = {
-            signInSuccessUrl: 'home.html',
-            signInOptions: [
-                {
-                    provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID
-                },
-                {
-                    provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
-                    requireDisplayName: true
-                }
-            ]
-        };
+     var database = firebase.database();
 
-        // Initialize the FirebaseUI Widget using Firebase.
-        var ui = new firebaseui.auth.AuthUI(firebase.auth());
-        // The start method will wait until the DOM is loaded.
-        ui.start('#firebaseui-auth-container', uiConfig);
+    function init() {
+        // login();
+        initEventHandlers();
+        addTrainToHTML()        
+    }
 
-        firebase.auth().onAuthStateChanged(function(user) {
-            if (user) {
-            }
-        }, function(error) {
-            console.log(error);
+    function initEventHandlers() {
+        $("#submitButton").on("click", function(event) {
+            trainName = $("#trainName").val().trim();
+            destination = $("#destination").val().trim();
+            firstTrainTime = $("#firstTrainTime").val().trim();
+            frequency = $("#frequency").val().trim();
+
+            pushDataToDatabase();
+        });
+    }
+
+    function pushDataToDatabase () {
+        database.ref().push({
+            trainName: trainName,
+            destination: destination,
+            firstTrainTime: firstTrainTime,
+            frequency: frequency
+        });
+        $(".myInput").val("");
+    }
+
+    function addTrainToHTML() {
+        database.ref().on("child_added", function(childSnapshot) {
+            newTrainData = childSnapshot.val();
+            $("#trainInfoTable").append(`
+                <tr>
+                    <td>${newTrainData.trainName}</td>
+                <tr>
+            `);
         });
     }
 
@@ -44,5 +63,53 @@ var LoginPage = function() {
 }();
 
 $(function() {
-    LoginPage.init()
+    TrainActivity.init();
 });
+
+
+
+
+
+
+
+
+
+    // function login() {
+    //      // Initialize Firebase
+    //   var config = {
+    //     apiKey: "AIzaSyAp6lBndBnWQIXB1uoGZEpJISR3YPKK8pM",
+    //     authDomain: "trainactivity-d3485.firebaseapp.com",
+    //     databaseURL: "https://trainactivity-d3485.firebaseio.com",
+    //     projectId: "trainactivity-d3485",
+    //     storageBucket: "",
+    //     messagingSenderId: "347867649628"
+    //   };
+    //   firebase.initializeApp(config);
+
+    // database = firebase.database();
+    //     // FirebaseUI config.
+    // var uiConfig = {
+    //     signInSuccessUrl: 'home.html',
+    //     signInOptions: [
+    //         {
+    //             provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID
+    //         },
+    //         {
+    //             provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    //             requireDisplayName: true
+    //         }
+    //     ]
+    // };
+
+    // // Initialize the FirebaseUI Widget using Firebase.
+    // var ui = new firebaseui.auth.AuthUI(firebase.auth());
+    // // The start method will wait until the DOM is loaded.
+    // ui.start('#firebaseui-auth-container', uiConfig);
+
+    // firebase.auth().onAuthStateChanged(function(user) {
+    //     if (user) {
+    //     }
+    // }, function(error) {
+    //     console.log(error);
+    // });
+    // }
